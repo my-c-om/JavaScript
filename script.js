d97b82757ff2bcf70628ebdd221b9bc2eb40751e
amkweb <athuyrain@gmail.com>
2025-03-28 12:24:30 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/auth_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    res.json({ message: "User Registered" });
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    if (user) {
+        res.json({ message: "Login Successful", user });
+    } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+    }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
